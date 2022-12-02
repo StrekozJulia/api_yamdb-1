db9896e4e96310d65c3334e8479bf598cde34e3c
Ilian Lypota <ilianlypota@gmail.com>
2022-12-02 11:16:24 +0300

    small review changes

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import ReceiveToken, SignUp
