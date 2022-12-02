5edf1ef71f7641302e04933da7bf818f10e0fa98
Ilian Lypota <ilianlypota@gmail.com>
2022-12-02 19:31:33 +0300

    new constant for length

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+SLUG_LEN = 50
+NAME_LEN = 256
+USER_LEN = 150
+
 
 class User(AbstractUser):
     """Кастомная модель для юзера"""
@@ -15,12 +19,12 @@ class User(AbstractUser):
         (USER, 'Пользователь'),
     ]
 
-    username = models.CharField('Имя пользователя', max_length=150,
+    username = models.CharField('Имя пользователя', max_length=USER_LEN,
                                 unique=True, blank=False, null=False)
-    email = models.EmailField('Электронная почта', max_length=254, unique=True,
-                              blank=False, null=False)
-    first_name = models.CharField('Имя', max_length=150, blank=True)
-    last_name = models.CharField('Фамилия', max_length=150,
+    email = models.EmailField('Электронная почта', max_length=NAME_LEN,
+                              unique=True, blank=False, null=False)
+    first_name = models.CharField('Имя', max_length=USER_LEN, blank=True)
+    last_name = models.CharField('Фамилия', max_length=NAME_LEN,
                                  blank=True)
     bio = models.TextField('Биография', blank=True)
     role = models.CharField('Роль',
